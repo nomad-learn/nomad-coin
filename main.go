c90d8182e01fa90c75bcfba781945fa122795279
cuttleman <silluat11@gmail.com>
2022-11-24 14:33:22 +0900

    feat: blockchain first architecture

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"fmt"
 
-	"github.com/nomad-learn/nomad-coin/person"
+	"github.com/nomad-learn/nomad-coin/blockchain"
 )
 
 func main() {
-	juno := person.Person{}
-	juno.SetDetails("nico", 12)
-	fmt.Println(juno)
+	blockchain := blockchain.GetBlockchain()
+	blockchain.AddBlock("Second BLock")
+	blockchain.AddBlock("Third BLock")
+
+	for _, block := range blockchain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
+	}
 }
